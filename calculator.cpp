f44cc9c70ee64645d925249306cff92f64f4de48
OleksiiSamutin <samutin63>
2020-09-27 21:31:41 +0300

    really cool commit

--- calculator.cpp
+++ calculator.cpp
@@ -1,5 +1,13 @@
 #include "calculator.h"
+/*
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc volutpat nec nisi at imperdiet. Duis lacinia dui eu dolor maximus viverra. Nulla est odio, suscipit ut nisi id, semper venenatis lorem. Curabitur leo magna, venenatis non purus id, convallis pulvinar lectus. Sed consectetur mollis tortor fermentum feugiat. Ut mi nulla, maximus vitae justo et, consequat pharetra nunc. Donec hendrerit risus id convallis cursus. Morbi laoreet eros nec dolor lobortis, cursus ornare lectus molestie. Vivamus sed massa neque. Ut auctor semper mauris, congue cursus leo placerat sit amet. Ut volutpat diam eget congue facilisis. Praesent vel blandit libero. Morbi scelerisque, orci a porta congue, neque dolor commodo nibh, non eleifend metus quam id elit. Proin facilisis nulla est, ac suscipit turpis interdum pharetra. Nunc malesuada diam ac elit dictum, at ullamcorper mi posuere.
 
+Pellentesque eu dignissim tellus. Etiam vel varius dolor, id ullamcorper odio. Morbi ornare nec ex condimentum pretium. Duis non ornare arcu. Cras placerat tellus ac mi auctor semper. Mauris et lacinia mauris, ut elementum lectus. Nulla faucibus sapien ac libero scelerisque, sit amet mattis dui luctus. Nunc vestibulum tortor nec massa semper, nec hendrerit metus placerat. Nulla erat tortor, eleifend ac blandit ac, placerat non enim. Nullam non commodo neque.
+
+Curabitur maximus, velit a sodales lacinia, odio enim placerat arcu, non viverra odio augue sed odio. Phasellus quis erat non orci vulputate dapibus. Donec porttitor purus ac elit sodales auctor. Integer consectetur lorem a enim aliquam iaculis. Vestibulum a urna vitae elit blandit pretium. Curabitur orci justo, sollicitudin in eros id, suscipit convallis libero. Curabitur quam sapien, porta at ex id, malesuada ultrices erat. Donec viverra lacus at rhoncus varius. Cras non maximus nisl. Donec libero nisi, efficitur vitae tincidunt quis, rhoncus ut sem. Nullam laoreet ligula blandit felis malesuada, id facilisis ipsum commodo. Phasellus non dictum ipsum. Nunc arcu purus, pellentesque nec hendrerit quis, finibus eget est. Duis finibus fermentum mollis. Quisque congue elementum metus ac cursus.
+
+Sed urna nibh, vehicula quis aliquet at, fringilla sed mi. Donec fermentum eget lectus ac vehicula. Vestibulum auctor eget nulla ac rhoncus. Cras laoreet sem eget risus vulputate scelerisque. Ut in faucibus purus. Duis tellus lacus, fermentum in scelerisque nec, egestas sit amet dolor. Integer eu laoreet augue, a porta eros. Nulla ut euismod est, in eleifend lacus. Pellentesque egestas leo vitae lectus facilisis condimentum. Integer ultrices risus purus, quis congue nisi elementum molestie. Pellentesque in bibendum orci.
+*/
 int Calculator::Add (double a, double b)
 {
 
